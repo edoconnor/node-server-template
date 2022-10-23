b8af15e2929706d02f3124cff71889d1a33ef0bb
Ed O'Connor <ed@oconnor.app>
2022-10-23 17:32:50 -0400

    initial

---
+++ src/state.ts
@@ -0,0 +1,10 @@
+import * as mongodb from "mongodb";
+
+export interface State {
+  name: string;
+  capital: string;
+  postal: string;
+  standard: string;
+  plates: string;
+  _id?: mongodb.ObjectId;
+}
